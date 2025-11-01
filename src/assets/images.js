1c03a75ca81ea7246228b6458a489ed86bc2ee57
Aryan Mehta <mehtaaryan2001@gmail.com>
2025-11-02 00:00:14 +0530

    📦 added export file for structured asset imports

---
+++ src/assets/images.js
@@ -0,0 +1,61 @@
+import Caline_Vito from "./caline-vito.jpg";
+import Sad_Vito from "./sad-vito.png";
+import Team_1 from "./Team-1.jpg";
+import Team_2 from "./Team-2.jpg";
+import Team_3 from "./Team-3.jpg";
+import Team_4 from "./Team-4.jpeg";
+import Vito_Story from "./Vito-Story.jpeg";
+import vitoviktor_logo from "./VItovikto_logo.svg";
+import vitoviktor_coin_1 from "./Vitoviktor-coin-1.png";
+import vitoviktor_coin_2 from "./vitoviktor-coin-2.png";
+import vitoviktor_freezing from "./vitoviktor-freezing-vito.png";
+import vitoviktor_happy_face from "./vitoviktor-happy-face.png";
+import vitoviktor_happyface from "./vitoviktor-happyface.png";
+import vitoviktor_Hero_section_background_image from "./vitoviktor-Hero-section-background-image.jpg";
+import vitoviktor_listening_to_music from "./vitoviktor-listening-to-music.png";
+import vitoviktor_music from "./vitoviktor-music.png";
+import Vitoviktor_NFT_collection_1 from "./Vitoviktor-NFT-collection-1.jpg";
+import Vitoviktor_NFT_collection_2 from "./Vitoviktor-NFT-collection-2.jpg";
+import Vitoviktor_NFT_collection_3 from "./Vitoviktor-NFT-collection-3.jpg";
+import Vitoviktor_NFT_collection_4 from "./Vitoviktor-NFT-collection-4.jpg";
+import Vitoviktor_NFT_collection_5 from "./Vitoviktor-NFT-collection-5.jpg";
+import Vitoviktor_NFT_collection_6 from "./Vitoviktor-NFT-collection-6.jpg";
+import Vitoviktor_NFT_collection_7 from "./Vitoviktor-NFT-collection-7.jpg";
+import Vitoviktor_NFT_collection_8 from "./Vitoviktor-NFT-collection-8.jpg";
+import Vitoviktor_NFT_collection_9 from "./Vitoviktor-NFT-collection-9.jpg";
+import Vitoviktor_NFT_collection_10 from "./Vitoviktor-NFT-collection-10.jpg";
+import Vitoviktor_NFT_collection_11 from "./Vitoviktor-NFT-collection-11.jpg";
+import Vitoviktor_NFT_collection_12 from "./Vitoviktor-NFT-collection-12.jpg";
+import Vitoviktor_superman_hero_section from "./Vitoviktor-superman-hero-section.png";
+
+export {
+  Caline_Vito,
+  Sad_Vito,
+  Team_1,
+  Team_2,
+  Team_3,
+  Team_4,
+  Vito_Story,
+  vitoviktor_logo,
+  vitoviktor_coin_1,
+  vitoviktor_coin_2,
+  vitoviktor_freezing,
+  vitoviktor_happy_face,
+  vitoviktor_happyface,
+  vitoviktor_Hero_section_background_image,
+  vitoviktor_listening_to_music,
+  vitoviktor_music,
+  Vitoviktor_NFT_collection_1,
+  Vitoviktor_NFT_collection_2,
+  Vitoviktor_NFT_collection_3,
+  Vitoviktor_NFT_collection_4,
+  Vitoviktor_NFT_collection_5,
+  Vitoviktor_NFT_collection_6,
+  Vitoviktor_NFT_collection_7,
+  Vitoviktor_NFT_collection_8,
+  Vitoviktor_NFT_collection_9,
+  Vitoviktor_NFT_collection_10,
+  Vitoviktor_NFT_collection_11,
+  Vitoviktor_NFT_collection_12,
+  Vitoviktor_superman_hero_section,
+};
