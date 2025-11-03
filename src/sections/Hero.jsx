4185687d7c53db4e3f1e688861aeb92418d9b5b3
Aryan Mehta <mehtaaryan2001@gmail.com>
2025-11-03 14:35:17 +0530

    ✨ feat: added header component with clean UI and motion effects

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { vitoviktor_Hero_section_background_image } from "../assets/images";
+import Header from "../components/Header";
+
 function Hero() {
   return (
     <div
-      className="h-screen w-full bg-cover bg-center"
+      className="h-auto w-full bg-cover bg-center flex justify-start"
       style={{
         backgroundImage: `url(${vitoviktor_Hero_section_background_image})`,
       }}
     >
+       
+      <Header />
         
     </div>
   );
