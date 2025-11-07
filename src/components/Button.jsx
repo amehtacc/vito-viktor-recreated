169e5f0cb3458ddc162eb61b78f4dd49662857e3
Aryan Mehta <mehtaaryan2001@gmail.com>
2025-11-07 13:01:34 +0530

    🎨 style: updated button component dimensions and added page background color

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-function Button({text}) {
+function Button({text, className}) {
   return (
     <div
-      className={`w-40 h-14 bg-black rounded-full border-none cursor-pointer group`}
+      className={`w-auto h-[57.2px] bg-black rounded-full border-none cursor-pointer group ${className}`}
     >
-      <button className="w-full h-full text-xl -translate-1 group-hover:translate-1 bg-white border-2 border-black rounded-full px-7 py-3 cursor-pointer transition-all duration-300">
+      <button className="w-full h-full text-xl -translate-1 group-hover:translate-1 bg-white border-2 border-black rounded-full px-[30px] py-3 cursor-pointer transition-all duration-300">
         {text}
       </button>
     </div>
