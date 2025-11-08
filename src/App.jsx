a165ebeddffe95079d9d052ded0f9b54193cccf0
Aryan Mehta <mehtaaryan2001@gmail.com>
2025-11-09 01:04:09 +0530

    ✨ feat: added socials section to the page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
+import Socials from "./sections/Socials";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <main>
         <Hero />
         <About />
+        <Socials />
       </main>
     </>
   );
