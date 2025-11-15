a7ca3af7f1110712a8999c7f37a2cf291ced6485
Aryan Mehta <mehtaaryan2001@gmail.com>
2025-11-15 21:59:09 +0530

    🎛️ fix: updated audio play button to use fixed/relative positioning depending on view

--- src/components/VitoSongPlay.jsx
+++ src/components/VitoSongPlay.jsx
@@ -4,9 +4,14 @@ import {
   vitoviktor_listening_to_music,
   viktor_music,
 } from "../assets/images";
+import { useInView } from "motion/react";
+
 function VitoSongPlay() {
   const [play, setPlay] = useState(false);
   const audioRef = useRef(null);
+  
+  const playerRef = useRef(null);
+  const isPlayerInView = useInView(playerRef)
 
   function playAudio() {
     setPlay((prev) => !prev);
@@ -19,11 +24,11 @@ function VitoSongPlay() {
   }
 
   return (
-    <div className="w-full pt-12 pl-[1.8rem]">
-      <div className="w-full">
+    <div ref={playerRef} className="w-full pt-12 pl-[1.8rem]">
+      <div className={`w-full ${isPlayerInView ? "relative" : "fixed bottom-6 z-50"}`}>
         <div className="max-w-fit flex items-center justify-center border border-[rgb(8,54,66)] rounded-full px-4 py-2 gap-6">
           <img
-            className="w-16 h-16"
+            className="w-14 xl:w-16 h-14 xl:h-16"
             src={play ? vitoviktor_listening_to_music : vitoviktor_music}
             alt="vitoviktor_music"
           />
@@ -57,7 +62,7 @@ function VitoSongPlay() {
             ></div>
           </div>
           <img
-            className="w-[42px] h-[42px] cursor-pointer"
+            className="w-9 xl:w-[42px] h-9 xl:h-[42px] cursor-pointer"
             src={
               play
                 ? "https://vitoviktor.be/wp-content/uploads/2025/08/pause-button-blue.svg"
